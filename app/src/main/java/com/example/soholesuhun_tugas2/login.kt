9c8513b7ce91dca5310cf5ae3cf48a9133191dec
soholesuhun <soholesuhun@gmail.com>
2025-03-15 11:49:04 +0900

    Update login.kt

--- app/src/main/java/com/example/soholesuhun_tugas2/login.kt
+++ app/src/main/java/com/example/soholesuhun_tugas2/login.kt
@@ -13,29 +13,7 @@ class login : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
-        val forgot = findViewById<TextView>(R.id.text_Forgot)
-
-        val btnLogin= findViewById<Button>(R.id.btnLogin)
-
-        val btnRegister= findViewById<TextView>(R.id.text_signUP)
-
-        // Ketika tombol diklik, pindah ke LoginActivity
-
-        btnLogin.setOnClickListener {
-            val intent = Intent(this, home::class.java)
-            startActivity(intent)
-        }
-
-        forgot.setOnClickListener {
-            val intent = Intent(this, resetPassword::class.java)
-            startActivity(intent)
-        }
-
-        // Ketika tombol diklik, pindah ke LoginActivity
-        btnRegister.setOnClickListener {
-            val intent = Intent(this, register::class.java)
-            startActivity(intent)
-        }
+    
 
     }
 }
